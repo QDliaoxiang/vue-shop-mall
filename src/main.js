b64316443f702d862fd055cb61c574a7a3742324
QDliaoxiang <2668812066@qq.com>
2020-03-06 13:44:17 +0800

    style init

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import "../src/assets/styles/reset.css"
+import "../src/assets/styles/border.css"
+
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)//解决移动端300毫秒延迟
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
