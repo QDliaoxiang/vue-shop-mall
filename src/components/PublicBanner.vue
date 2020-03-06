b25e44f5a832d762b44dee924cbeb75f3036d58e
QDliaoxiang <2668812066@qq.com>
2020-03-06 17:48:50 +0800

    add public component

---
+++ src/components/PublicBanner.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h3 class="title">{{bannerSrc.title}}</h3>
+    <div class="show">
+      <img class="ban-img" :src="bannerSrc.img" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PublicBanner',
+  props: ['bannerSrc'],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .title
+    padding: .1rem;
+    color: #666;
+  .show
+    width: 100%;
+    height: 1rem;
+    .ban-img
+      width: 100%;
+      
+</style>
--- src/views/pages/home/Home.vue
+++ src/views/pages/home/Home.vue
@@ -3,7 +3,7 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
-  
+    <public-banner :bannerSrc="bans[0]"></public-banner>
   </div>
 </template>
 
@@ -11,12 +11,27 @@
 import HomeHeader from "./components/HomeHeader";
 import HomeSwiper from "./components/HomeSwiper";
 import HomeIcons from "./components/HomeIcons"
+import PublicBanner from "../../../components/PublicBanner"
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
     HomeIcons,
+    PublicBanner,
+  },
+  data () {
+    return {
+      bans:[
+        {
+          title: '每日特价',
+          img: '//gw.alicdn.com/imgextra/i4/1444798/O1CN01geJ5JY1lJWuaG6X4e_!!1444798-0-lubanu.jpg'
+        },
+        {
+          img: '//gw.alicdn.com/imgextra/i2/99/O1CN01rM3hU51CbNnUE5rLP_!!99-0-lubanu.jpg'
+        }
+      ]
+    }
   }
 }
 </script>
