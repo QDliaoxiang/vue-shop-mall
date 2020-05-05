8271425a6d0eb34893f86b5534f30ba08c94396e
QDliaoxiang <2668812066@qq.com>
2020-05-05 17:24:21 +0800

    购物车无商品添加小提示

--- src/views/pages/shopCart/ShopCart.vue
+++ src/views/pages/shopCart/ShopCart.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="wrap">
     <cart-header></cart-header>
-    <product-item v-for="item in cartList" :item="item" :key="item.id"></product-item>
+    <div v-if="ishasProduct">
+      <product-item v-for="item in cartList" :item="item" :key="item.id"></product-item>
+    </div>
+    <div v-if="!ishasProduct" class="empty">购物车空空如也，去加点商品再来吧！</div>
   </div>
 </template>
 <script>
@@ -17,10 +20,23 @@ export default {
     return {
       cartList: this.$store.state.shopcart
     }
+  },
+  computed: {
+    ishasProduct() {
+      if(this.cartList.length > 0) {
+        return true
+      }else{
+        return false
+      }
+    }
   }
 }
 </script>
 <style lang="stylus" scoped>
 .wrap
   padding-top: 1rem;
+  .empty
+    text-align: center
+    margin-top: 1rem
+    color: #ccc
 </style>
